957627a97454c85f14f3ef75944e06ad4d0ba172
blaze <blaze@DESKTOP-AB340BE>
2024-11-10 20:41:39 -0300

    carga de base de tp8- chincha luis M.

---
+++ src/main/java/ar/edu/ies6/Tp08Grupo1Ies6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp08Grupo1Ies6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp08Grupo1Ies6Application.class, args);
+	}
+
+}
