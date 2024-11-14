d69a915034506152606a1eca2a40e190c0f3f60f
PC-RETRO <PC-RETRO@PC-RETRO>
2024-11-14 19:18:35 -0300

    añadiendo Util, y Repository joelcalatayud

---
+++ src/main/java/ar/edu/ies6/repository/ProductoRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.ies6.repository;
+
+public class ProductoRepository {
+
+	public interface ProductoRepository extends CrudRepository <Producto, String>
+
+}
---
+++ src/main/java/ar/edu/ies6/util/AlmacenProducto.java
@@ -0,0 +1,10 @@
+package ar.edu.ies6.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.ies6.model.Producto;
+
+public class AlmacenProducto {
+	public static List<Producto> productos = new ArrayList<Producto>(); 
+}
